a5cfcfecf732a7f2c3cfd83562d12db2c88de92a
kinarashah <kinarahs@usc.edu>
2018-05-24 09:53:14 -0700

    init

--- pkg/api/customization/node/node.go
+++ pkg/api/customization/node/node.go
@@ -5,6 +5,13 @@ import (
 	"github.com/rancher/norman/types/convert"
 	"github.com/rancher/types/apis/management.cattle.io/v3"
 	"github.com/rancher/types/client/management/v3"
+	managementschema "github.com/rancher/types/apis/management.cattle.io/v3/schema"
+	"github.com/rancher/norman/api/access"
+	"github.com/rancher/norman/httperror"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"github.com/rancher/norman/types/values"
+	"fmt"
+	"github.com/rancher/rancher/utils"
 )
 
 // Formatter for Node
@@ -31,4 +38,30 @@ func Formatter(apiContext *types.APIContext, resource *types.RawResource) {
 	if nodeTemplateID == nil && customConfig == nil {
 		delete(resource.Links, "remove")
 	}
+
+	resource.AddAction(apiContext, "cordon")
+}
+
+type ActionWrapper struct {}
+
+func (a ActionWrapper) ActionHandler(actionName string, action *types.Action, apiContext *types.APIContext) error {
+	switch actionName {
+	case "cordon":
+		var node map[string]interface{}
+		if err := access.ByID(apiContext, apiContext.Version, apiContext.Type, apiContext.ID, &node); err != nil {
+			return httperror.NewAPIError(httperror.InvalidReference, "Error accessing node")
+		}
+		schema := apiContext.Schemas.Schema(&managementschema.Version, client.NodeType)
+		unschedulable := convert.ToBool(values.GetValueN(node, "unschedulable"))
+		if unschedulable {
+			return httperror.NewAPIError(httperror.InvalidAction, fmt.Sprintf("Node %s already cordoned", apiContext.ID))
+		}
+		values.PutValue(node, true, "unschedulable")
+		updated, err := schema.Store.Update(apiContext, schema, node, apiContext.ID)
+		if err != nil && apierrors.IsNotFound(err) {
+			return httperror.NewAPIError(httperror.ServerError, fmt.Sprintf("Error updating node %s by %s : %s", apiContext.ID, actionName, err.Error()))
+		}
+		utils.Myprint(updated, "updated .. ")
+	}
+	return nil
 }
